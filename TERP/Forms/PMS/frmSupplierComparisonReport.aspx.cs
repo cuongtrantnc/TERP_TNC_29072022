e850c2564d76a8524fcd721f3fa990987c910d77
Dev-vohuunhat <nhat.vo@thinknextco.com>
2022-08-06 14:23:40 +0700

    Thực hiện thiết kế giao diện menu SupplierComparisonReport và thiết kế giao diện cho báo

---
+++ TERP/Forms/PMS/frmSupplierComparisonReport.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+
+namespace TERP.Forms.PMS
+{
+    public partial class frmSupplierComparisonReport : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string rptFile = "/terp/Report/SupplierComparison.rpt";
+            ReportDocument crystalReport;
+
+            crystalReport = new ReportDocument();
+            crystalReport.Load(Server.MapPath(rptFile));
+
+
+            CrystalReportViewer.ReportSource = crystalReport;
+        }
+    }
+}
