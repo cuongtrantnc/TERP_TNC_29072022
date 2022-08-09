8ff7044b2345e8110e93cff30d6a7458c7ad1a77
Dev-vohuunhat <nhat.vo@thinknextco.com>
2022-08-09 12:07:05 +0700

    Tạo mới và thiết kế giao diện menu Criteria Supplier Performance Maint

--- TERP/Forms/PMS/frmCriteriaSupplierPerformanceMaint.aspx.cs
+++ TERP/Forms/PMS/frmCriteriaSupplierPerformanceMaint.aspx.cs
@@ -35,5 +35,32 @@ namespace TERP.Forms.PMS
         {
             ScriptManager.RegisterStartupScript(this, this.GetType(), "#CriteriaMaintForm", "showCriteriaSupplier_Form();", true);
         }
+
+        protected void grCriteriaSupplierPerformanceMaint_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if(e.Row.RowType == DataControlRowType.DataRow)
+            {
+                LinkButton label = (LinkButton)e.Row.FindControl("btnChangeStatus");
+
+                if (label.Text.Trim() == "Active")
+                {
+                    label.CssClass = "bagde-custom_success";
+                }
+                else
+                {
+                    label.CssClass = "bagde-custom_danger";
+                }
+            }
+        }
+
+        protected void btnDeleteCriteriaMaint_Click(object sender, ImageClickEventArgs e)
+        {
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "#CriteriaModalDelete", "showCriteriaDeleteModal();", true);
+        }
+
+        protected void btnChangeStatus_Click(object sender, EventArgs e)
+        {
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "#CriteriaModalUpdate", "showCriteriaUpdateModal();", true);
+        }
     }
 }
